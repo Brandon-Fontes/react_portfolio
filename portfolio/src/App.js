201b44cebb90d96b75defcc0929c3e7482087737
DaFonz <brandonjfontes@gmail.com>
2021-03-26 19:46:08 -0600

    initial commit

---
+++ portfolio/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Nav from './Nav';
+import About from './About';
+
+
+
+function App() {
+  return (
+    <>
+    <Nav />
+    <About />
+    </>
+  );
+}
+
+export default App;
